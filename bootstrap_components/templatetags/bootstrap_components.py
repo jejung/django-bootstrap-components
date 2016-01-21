533091efc939045685424ae58e2040373c81cd0b
jejung <jean.jung@rocketmail.com>
2016-01-21 18:14:21 -0200

    Including tag configuration for bootstrap_pages

--- bootstrap_components/templatetags/bootstrap_components.py
+++ bootstrap_components/templatetags/bootstrap_components.py
@@ -1,3 +1,7 @@
 from django import template
 
 register = template.Library()
+
+@register.inclusion_tag('bootstrap/bootstrap_page.html', takes_context=True)
+def bootstrap_page(context):
+    return context
---
+++ build-install.bat
@@ -0,0 +1,2 @@
+python setup.py sdist
+python -m pip install --upgrade dist/django-bootstrap-components-0.1.zip
